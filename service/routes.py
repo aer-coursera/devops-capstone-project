a253cdf6276d118a4903c97a1af8d2e75b266360
aer-coursera <m.woehr@klosterkind.com>
2023-06-17 16:57:53 -0400

    corrected lint

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -71,6 +72,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -89,6 +91,7 @@ def update_accounts(account_id):
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -103,6 +106,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # READ AN ACCOUNT
